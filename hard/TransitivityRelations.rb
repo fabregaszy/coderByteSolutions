68d41fea510623b1eb6a6c457017f2c635c54a8e
fabregaszy <yanzhang1989@icloud.com>
2014-05-18 14:50:00 +0800

    first edition with BUGS in it

--- hard/TransitivityRelations.rb
+++ hard/TransitivityRelations.rb
@@ -13,6 +13,36 @@ def strToArray strArr
   arr
 end
 
+def TransitivityRelations strArr
+  transitive = true
+  connect_needed = ""
+  arr = strToArray strArr
+  # it's a n * n matrix
+  n = arr.length
+  for i in 0..n-1 do
+    for j in 0..n-1 do
+      if arr[i][j] == 1 then
+        for k in 0..n-1 do
+          if arr[j][k] == 1 then
+            if arr[i][k] == 0 then
+              transitive = false
+              connect_needed << "(#{i},#{k})-"
+              arr[i][k] = 1
+            end
+          end
+        end
+      end
+    end
+  end
+  if transitive then
+    "transitive"
+  else
+    connect_needed[0..-2] # drop the last '-'
+  end
+end
+
 
 # test drive
-p strToArray ["(1,1,1)", "(1,0,0)", "(0,1,0)"]
+p TransitivityRelations ["(1,1,1)", "(1,0,0)", "(0,1,0)"]
+p TransitivityRelations ["(0,1,0)", "(1,0,1)", "(0,1,0)"]
+p TransitivityRelations ["(0,1,0,0)", "(0,0,1,0)", "(0,0,1,1)", "(0,0,0,1"]
